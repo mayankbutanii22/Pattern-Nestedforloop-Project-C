4817f80cdeea8ff386d1029c75202a5e7a998ab5
mayankbutanii22 <162984156+mayankbutanii22@users.noreply.github.com>
2024-05-03 23:57:53 +0530

    Add files via upload

---
+++ nestedforloop-pattern3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j,k;
+   for(i=5;i>=1;i--)
+   {
+         for(k=1;k<=5-i+1;k++)
+         {
+       printf("  ");
+         }
+       for(j=i;j>=1;j--)
+       {
+       printf("%d",j); 
+       }
+       printf("\n");
+   }
+}
